72c42c4086b9651425119338e13b691210b427be
Sophie <sophiehasindrae@gmail.com>
2022-09-21 15:23:35 +0300

    changement de v1 et v2

--- version1.py
+++ version1.py
@@ -1,48 +1,55 @@
-from tkinter import *
+from tkinter import Canvas
+from tkinter import Tk
+from tkinter import Button
+from tkinter import X
+from tkinter import Frame
 from tkinter import messagebox
-class manuel:
-    def __init__(self,i,fenetre):
-        self.i = 1
-        self.fenetre = fenetre
+class feu_tri_color:
+    def __init__(self,position_feu,fenetre_principal):
+        self.position_feu = position_feu
+        self.fenetre_principal = fenetre_principal
     def suivant(self):
-        if(self.i == 1):
-            dessin.itemconfigure(rouge, fill = "gray")
-            dessin.itemconfigure(jaune, fill = "yellow")
-            self.i  = 2
-        elif(self.i == 2):
-            dessin.itemconfigure(vert, fill = "green")
-            dessin.itemconfigure(jaune, fill = "gray")
-            self.i  = 3
-        elif(self.i == 3):
-            dessin.itemconfigure(rouge, fill = "red")
-            dessin.itemconfigure(vert, fill ="gray")
-            self.i  = 1
+        if(self.position_feu == 1):
+            dessin.itemconfigure(feu_rouge, fill = "gray")
+            dessin.itemconfigure(feu_jaune, fill = "yellow")
+            self.position_feu  = 2
+        elif(self.position_feu == 2):
+            dessin.itemconfigure(feu_vert, fill = "green")
+            dessin.itemconfigure(feu_jaune, fill = "gray")
+            self.position_feu  = 3
+        elif(self.position_feu == 3):
+            dessin.itemconfigure(feu_rouge, fill = "red")
+            dessin.itemconfigure(feu_vert, fill ="gray")
+            self.position_feu  = 1
 
     def initialiser(self):
         
-        dessin.itemconfigure(rouge, fill = "red")
-        dessin.itemconfigure(vert, fill = "gray")
-        dessin.itemconfigure(jaune, fill = "gray")
-        self.i = 1
+        dessin.itemconfigure(feu_rouge, fill = "red")
+        dessin.itemconfigure(feu_vert, fill = "gray")
+        dessin.itemconfigure(feu_jaune, fill = "gray")
+        self.position_feu = 1
         
     def confirm(self):
         reponse = messagebox.askyesnocancel("Question", "Voulez-vous continuer ?")
         if(reponse == True):
-            self.fenetre.destroy()
+            self.fenetre_principal.destroy()
+            
 if __name__ == '__main__':
-    fen = Tk()
-    man = manuel(1,fen)
-    dessin = Canvas(fen, width =200, height =500, bg ="ivory")
+    fenetre_principal = Tk()
+    position_feu_init = 1
+    feu_tri_color = feu_tri_color(position_feu_init,fenetre_principal)
+    dessin = Canvas(fenetre_principal, width =200, height =500, bg ="ivory")
     dessin.grid(row = 0, column = 0, rowspan = 2,padx = 5, pady = 5)
-    rouge = dessin.create_oval(40,50,137,147, fill = "red")
-    jaune = dessin.create_oval(40,177,137,274, fill = "gray")
-    vert = dessin.create_oval(40,304,137,401, fill = "gray")
-    zone = Frame(fen, bg='#777777')
-    zone.grid(row=0,column=1,ipadx=1,ipady = 5)
+    
+    feu_rouge = dessin.create_oval(40,50,137,147, fill = "red")
+    feu_jaune = dessin.create_oval(40,177,137,274, fill = "gray")
+    feu_vert = dessin.create_oval(40,304,137,401, fill = "gray")
+    zone_bouton = Frame(fenetre_principal, bg='#777777')
+    zone_bouton.grid(row=0,column=1,ipadx=1,ipady = 5)
 
-    btn_next  = Button(zone, text = "Suivant", command = man.suivant)
-    btn_next.pack(fill=X)
-    btn_prev = Button(zone, text = "Initialiser", command = man.initialiser)
-    btn_prev.pack(fill=X)
-    btn_quitter = Button(zone, text = "Quitter", command = man.confirm)
-    btn_quitter.pack(fill=X)
+    bouton_suivant  = Button(zone_bouton, text = "Suivant", command = feu_tri_color.suivant)
+    bouton_suivant.pack(fill=X)
+    bouton_initialiser = Button(zone_bouton, text = "Initialiser", command = feu_tri_color.initialiser)
+    bouton_initialiser.pack(fill=X)
+    bouton_quitter = Button(zone_bouton, text = "Quitter", command = feu_tri_color.confirm)
+    bouton_quitter.pack(fill=X)
--- version2.py
+++ version2.py
@@ -1,74 +1,77 @@
-from tkinter import *
-from tkinter import messagebox
-from version1 import *
+
+from tkinter import Radiobutton
+from tkinter import X
+from tkinter import Label
+from tkinter import Tk
+from tkinter import Frame
+from tkinter import Canvas
+from tkinter import Scale
+from tkinter import IntVar
+from tkinter import VERTICAL
 import time
-class version2:
+class feu_tri_color_v2:
     def __init__(self,fenetre):
         self.fenetre = fenetre
         
-    def manuel2(self):
-        manuel(1,self.fenetre)
+    def manuel(self):
+        #manuel(1,self.fenetre)
+        print("salut")
         
-    def auto(self):
-        btn_man.pack_forget()
-        btn_auto.pack_forget()
-        l1.pack(fill = X)
-        sc.pack(fill=X)
-        l2.pack(fill = X)
-        while True:
-            dessin.itemconfigure(rouge, fill = "red")
-            dessin.itemconfigure(jaune, fill = "gray")
-            dessin.itemconfigure(vert, fill = "gray")
-            time.sleep(1)
-            dessin.itemconfigure(jaune, fill = "yellow")
-            dessin.itemconfigure(rouge, fill = "gray")
-            dessin.itemconfigure(vert, fill = "gray")
-            time.sleep(1)
-            dessin.itemconfigure(vert, fill = "green")
-            dessin.itemconfigure(rouge, fill = "gray")
-            dessin.itemconfigure(jaune, fill = "gray")
-            time.sleep(1)
-
+    def changement_couleur(self,duree_allumage):
+        dessin.itemconfigure(feu_rouge, fill = "red")
+        dessin.itemconfigure(feu_jaune, fill = "gray")
+        dessin.itemconfigure(feu_vert, fill = "gray")
+        time.sleep(duree_allumage)
+        dessin.itemconfigure(feu_jaune, fill = "yellow")
+        dessin.itemconfigure(feu_rouge, fill = "gray")
+        dessin.itemconfigure(feu_vert, fill = "gray")
+        time.sleep(duree_allumage)
+        dessin.itemconfigure(feu_vert, fill = "green")
+        dessin.itemconfigure(feu_rouge, fill = "gray")
+        dessin.itemconfigure(feu_jaune, fill = "gray")
+        time.sleep(duree_allumage)
         
-    def change_vitesse(self,v):
-        vitesse = int(v)
+    def automatique(self):
+        bouton_manuel.pack_forget()
+        bouton_automatique.pack_forget()
+        label_rapide.pack(fill = X)
+        echelle.pack(fill=X)
+        label_lent.pack(fill = X)
+        duree_initial = 1
         while True:
-            dessin.itemconfigure(rouge, fill = "red")
-            dessin.itemconfigure(jaune, fill = "gray")
-            dessin.itemconfigure(vert, fill = "gray")
-            time.sleep(vitesse)
-            dessin.itemconfigure(jaune, fill = "yellow")
-            dessin.itemconfigure(rouge, fill = "gray")
-            dessin.itemconfigure(vert, fill = "gray")
-            time.sleep(vitesse)
-            dessin.itemconfigure(vert, fill = "green")
-            dessin.itemconfigure(rouge, fill = "gray")
-            dessin.itemconfigure(jaune, fill = "gray")
-            time.sleep(vitesse)
+            changement_couleur(duree_initial)
+        
+    def change_vitesse(self,valeur_echelle):
+        duree_allumage = int(valeur_echelle)
+        while True:
+            changement_couleur(duree_allumage)
             
 
 if __name__ == '__main__':
-    fen  = Tk()
-    v = IntVar()
-    v2 = IntVar()
-    vers = version2(fen)
-    dessin = Canvas(fen, width =200, height =500, bg ="ivory")
+    fenetre_principal  = Tk()
+    variable_radio_btn = IntVar()
+    variable_echelle = IntVar()
+    feu_tri_color_v2 = feu_tri_color_v2(fenetre_principal)
+    dessin = Canvas(fenetre_principal, width =200, height =500, bg ="ivory")
     dessin.grid(row = 0, column = 0, rowspan = 2,padx = 5, pady = 5)
-    rouge = dessin.create_oval(40,50,137,147, fill = "red")
-    jaune = dessin.create_oval(40,177,137,274, fill = "gray")
-    vert = dessin.create_oval(40,304,137,401, fill = "gray")
-    zone = Frame(fen, bg='#777777')
-    zone.grid(row=0,column=1,ipadx=5,ipady = 5)
-    btn_man  = Radiobutton(zone, text = "Manuel", variable = v, command = vers.manuel2, value = 1)
-    btn_man.pack(fill=X)
-    btn_auto = Radiobutton(zone, text = "Automatique", variable = v, command = vers.auto, value = 2)
-    btn_auto.pack(fill=X)
-    l1 = Label(zone, text = "rapide")
-    l1.pack_forget()
-    sc = Scale( zone, variable = v2,
+    
+    feu_rouge = dessin.create_oval(40,50,137,147, fill = "red")
+    feu_jaune = dessin.create_oval(40,177,137,274, fill = "gray")
+    feu_vert = dessin.create_oval(40,304,137,401, fill = "gray")
+    
+    zone_bouton = Frame(fenetre_principal, bg='#777777')
+    zone_bouton.grid(row=0,column=1,ipadx=5,ipady = 5)
+    
+    bouton_manuel  = Radiobutton(zone_bouton, text = "Manuel", variable = variable_radio_btn, command = feu_tri_color_v2.manuel, value = 1)
+    bouton_manuel.pack(fill=X)
+    bouton_automatique = Radiobutton(zone_bouton, text = "Automatique", variable = variable_radio_btn, command = feu_tri_color_v2.automatique , value = 2)
+    bouton_automatique.pack(fill=X)
+    label_rapide = Label(zone_bouton, text = "rapide")
+    label_rapide.pack_forget()
+    echelle = Scale( zone_bouton, variable = variable_echelle,
            from_ = 100, to = 1,
            orient = VERTICAL)
-    sc.pack_forget()
-    sc.bind('<Button-1>', vers.change_vitesse(str(v2)))
-    l2 = Label(zone, text = "lent")
-    l2.pack_forget()
+    echelle.pack_forget()
+    echelle.bind('<Button-1>', feu_tri_color_v2.change_vitesse(str(variable_echelle)))
+    label_lent = Label(zone_bouton, text = "lent")
+    label_lent.pack_forget()
